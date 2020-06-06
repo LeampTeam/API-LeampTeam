22c108a42e7794bf5e8aaaa368420f58d4fd0ec2
RafaelZiraldo <rafazira83@gmail.com>
2020-06-06 11:16:09 -0300

    profile usuario

--- controller/usuario.js
+++ controller/usuario.js
@@ -1,53 +1,55 @@
 const bcrypt = require('bcryptjs');
 const Usuario=require('../model/usuario')
 let jwt=require('../servicio/jwt')
+var path = require('path');
+var fs = require('fs');
 
-function registrar(req,res){
-    let params=req.body;
-    let usuario=new Usuario();
+// function registrar(req,res){
+//     let params=req.body;
+//     let usuario=new Usuario();
 
-    if(params.email && params.password){
+//     if(params.email && params.password){
        
-        usuario.name='';
-        usuario.surname='';
-        usuario.email=params.email;
-        usuario.role='USUARIO';
-        usuario.img=null;
-        usuario.eliminado=false
+//         usuario.name='';
+//         usuario.surname='';
+//         usuario.email=params.email;
+//         usuario.role='USUARIO';
+//         usuario.img=null;
+//         usuario.eliminado=false
 
-        Usuario.find({email:usuario.email}).exec(function(err,users){
-                if(err){return res.status(500).send({message:'error en la peticion'})}
+//         Usuario.find({email:usuario.email}).exec(function(err,users){
+//                 if(err){return res.status(500).send({message:'error en la peticion'})}
 
-                if(users && users.length>=1){
-                    return res.status(200).send({message:'el usario ya existe'})
-                }else{
-                    bcrypt.genSalt(10, function(err, salt) {
-                        bcrypt.hash(params.password,salt,function(err,hash){
-                            usuario.password=hash
+//                 if(users && users.length>=1){
+//                     return res.status(200).send({message:'el usario ya existe'})
+//                 }else{
+//                     bcrypt.genSalt(10, function(err, salt) {
+//                         bcrypt.hash(params.password,salt,function(err,hash){
+//                             usuario.password=hash
             
-                            usuario.save((err,usuarioguardado)=>{
-                                if(err) return res.status(500).send({
-                                    message:'error al guardar el usuario',
-                                    err
-                                })
-                                if(usuarioguardado){
-                                    res.status(200).send({usuario:usuarioguardado})
-                                }else{
-                                    res.status(404).send({message:'No se registro el usario'})
-                                }
-                            })
-                        })
-                    })
-                }
-        })
+//                             usuario.save((err,usuarioguardado)=>{
+//                                 if(err) return res.status(500).send({
+//                                     message:'error al guardar el usuario',
+//                                     err
+//                                 })
+//                                 if(usuarioguardado){
+//                                     res.status(200).send({usuario:usuarioguardado})
+//                                 }else{
+//                                     res.status(404).send({message:'No se registro el usario'})
+//                                 }
+//                             })
+//                         })
+//                     })
+//                 }
+//         })
        
 
-    }else{
-        res.status(200).send({
-            message:'El usuario no fue registrado'
-        })
-    }
-}
+//     }else{
+//         res.status(200).send({
+//             message:'El usuario no fue registrado'
+//         })
+//     }
+// }
 
 function loguear(req,res){
     let params=req.body;
@@ -69,7 +71,7 @@ function loguear(req,res){
                         if(check){
                             // if(params.gettoken){
                                 return res.status(200).send({
-                                    token:jwt.createToken(user)
+                                    token:jwt.createToken(user),img:user.img,email:user.email
                                 })
                             // }else{
                             //     user.password=undefined
@@ -134,10 +136,62 @@ function loguear(req,res){
         break;
     }
 }
+function uploadImage(req, res) {
+    
+    let user=req.user
+
+    if (req.files) {
+        var file_path = req.files.file.path;
+        console.log(file_path)
+        var file_split = file_path.split('/');
+        console.log(file_split)
+        var file_name = file_split[2];
+        console.log(file_name)
+        var ext_split = file_name.split('.');
+        console.log(ext_split)
+        var file_ext = ext_split[1]
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+            Usuario.findByIdAndUpdate(user.sub, { img: file_name }, { new: true }, (err, usuarioUpdate) => {
+                if (err) return res.status(500).send({ message: 'Error en la peticion' })
+
+                if (!usuarioUpdate) return res.status(404).send({ message: 'No se ha podido Actualizar' })
+
+                return res.status(200).send('Imagen cargada')
+            })
+        } else {
+            removeFilesOfUploads(res, file_path, 'La extencion no es valida')
+
+        }
+    } else {
+        return res.status(200).send({ message: 'No se han subido archivos' })
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.img;
+    console.log(imageFile)
+    var pathFile = './imagenes/usuarios/' + imageFile
+
+    fs.exists(pathFile, (exists) => {
+        if (exists) {
+            res.sendFile(path.resolve(pathFile))
+        } else {
+            res.status(400).send({ message: 'El archivo no fue encotrado' })
+        }
+    })
+}
+function removeFilesOfUploads(res, file_path, message) {
+    fs.unlink(file_path, (err) => {
+        return res.status(200).send({ message: message })
+    })
+}
 
 
 module.exports={
-registrar,
-loguear
+
+loguear,
+getImageFile,
+uploadImage
 
 }
--- route/usuario.js
+++ route/usuario.js
@@ -3,9 +3,15 @@
 var express=require('express')
 var api=express.Router();
 var usuario=require('../controller/usuario')
+var multipart=require('connect-multiparty')
+var md_upload = multipart({uploadDir:'./imagenes/usuarios'})
+let md_auth=require('../middleware/authenticated')
 
 api.post('/loguin',usuario.loguear)
-api.post('/registrar',usuario.registrar)
+
+api.post('/cargarImagen',md_auth.ensureAuth,md_upload,usuario.uploadImage)
+api.get('/obtenerImagen/:img',usuario.getImageFile)
+
 
 
 
